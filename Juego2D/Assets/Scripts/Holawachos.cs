0fe0f4cc010442851d01fbde03f599b3caed0fdd
RubenGlezHS03 <r.gonzalez.hs03@gmail.com>
2022-05-17 09:59:08 +0200

    Hola wachos

---
+++ Juego2D/Assets/Scripts/Holawachos.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Holawachos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Me gusta Dani ^^");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
